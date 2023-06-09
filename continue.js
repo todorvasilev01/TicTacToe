c21bd2b315e094cf476a9eccef5e754419f22538
todorvasilev01 <totovasilevhr@abv.bg>
2023-06-09 11:13:15 +0300

    Create continue.js

---
+++ continue.js
@@ -0,0 +1,21 @@
+function continueTheGame(){
+    button1 = document.getElementById("button1").value = '';
+    button2 = document.getElementById("button2").value = '';
+    button3 = document.getElementById("button3").value = '';
+    button4 = document.getElementById("button4").value = '';
+    button5 = document.getElementById("button5").value = '';
+    button6 = document.getElementById("button6").value = '';
+    button7 = document.getElementById("button7").value = '';
+    button8 = document.getElementById("button8").value = '';
+    button9 = document.getElementById("button9").value = '';
+
+    button1 = document.getElementById("button1").disabled = false;
+    button2 = document.getElementById("button2").disabled = false;
+    button3 = document.getElementById("button3").disabled = false;
+    button4 = document.getElementById("button4").disabled = false;
+    button5 = document.getElementById("button5").disabled = false;
+    button6 = document.getElementById("button6").disabled = false;
+    button7 = document.getElementById("button7").disabled = false;
+    button8 = document.getElementById("button8").disabled = false;
+    button9 = document.getElementById("button9").disabled = false;
+}
